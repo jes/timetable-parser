4e3ff4deff4801a2b4d0d0d012b21721b48d5d80
James Stanley <james@incoherency.co.uk>
2012-04-12 14:38:38 +0100

    Make event generation cleaner (don't output exclusions for dates after the real end date) and stop outputting one extra week for each event

--- Timetable.pm
+++ Timetable.pm
@@ -21,7 +21,7 @@ use warnings;
 
 =head1 NAME
 
-Timetable - convert a University of Bath HTML timetable to an iCalendar file
+Timetable.pm - convert a University of Bath HTML timetable to an iCalendar file
 
 =head1 SYNOPSIS
 
@@ -37,9 +37,9 @@ Timetable - convert a University of Bath HTML timetable to an iCalendar file
 The University of Bath give timetables in HTML files which are needlessly
 difficult to use. For example:
 
-http://timetables.bath.ac.uk:4090/reporting/individual?identifier=Second+year+
-Chemistry+with+Management&weeks=19-32&idtype=name&objectclass=programme%2Bof%2B
-study%2Bgroups
+http://timetables.bath.ac.uk:4090/reporting/individual?identifier=Secon\
+d+year+Chemistry+with+Management&weeks=19-32&idtype=name&objectclass=pr\
+ogramme%2Bof%2Bstudy%2Bgroups
 
 This module is capable of scraping these pages and outputting iCalendar (RFC
 2445) files containing the events.
@@ -133,6 +133,37 @@ sub _count_cells {
     return $cells;
 }
 
+=begin private
+
+=head2 _utc_time_for $year, $month, $day, $weeks, $hour, $minute
+
+Return the UTC time for the Europe/London time which is $weeks weeks after the
+date given with ($year, $month, $day) and is at $hour:$minute.
+
+Return is of the form ($year, $month, $day, $hour, $minute).
+
+=end private
+
+=cut
+
+sub _utc_time_for {
+    my ($year, $month, $day, $weeks, $hour, $minute) = @_;
+
+    ($year, $month, $day) = Add_Delta_Days( $year, $month, $day, 7 * ($weeks - 1) );
+
+    my $d = DateTime->new(
+            year => $year,
+            month => $month,
+            day => $day,
+            hour => $hour,
+            minute => $minute,
+            time_zone => 'Europe/London',
+    );
+    $d->set_time_zone( 'UTC' );
+
+    return ($d->year, $d->month, $d->day, $d->hour, $d->minute);
+}
+
 =head2 ical_for_dom $start, $dom
 
 $start is as for ical_for_url.
@@ -182,11 +213,9 @@ sub ical_for_dom {
 
     my @events;
     my $ninputevents = 0;
-    my $minweek = -1;
-    my $maxweek = 1;
 
     # build list of events
-    my $dayofweek = 1;
+    my $dayofweek = 0;
     my $justincrementedday = 1;
     my $rowsuntilincrement = $table[1]->[0]->rowSpan;
     for (my $i = 1; $i < @table; $i++) {
@@ -210,27 +239,24 @@ sub ical_for_dom {
             $weeks =~ s/-/../g;
             my $range = Number::Range->new( $weeks );
 
-            $minweek = min( $range->range ) if $minweek == -1;
-            $minweek = min( $minweek, $range->range );
-            $maxweek = max( $maxweek, $range->range );
-
             my %event = ( 
                     time => $time,
                     hours => $hours,
                     subject => $subject,
                     room => $room,
                     weeks => $range,
+                    dayofweek => $dayofweek,
             );
 
-            push @{ $events[$dayofweek] }, \%event;
+            push @events, \%event;
             $ninputevents++;
         }
 
         $rowsuntilincrement--;
-        if ($i != @table - 1 && $rowsuntilincrement == 0) {
+        if ($rowsuntilincrement == 0) {
             $dayofweek++;
             $justincrementedday = 1;
-            $rowsuntilincrement = $table[$i+1]->[0]->rowSpan;
+            $rowsuntilincrement = $table[$i+1]->[0]->rowSpan if $i+1 != @table;
         } else {
             $justincrementedday = 0;
         }
@@ -242,76 +268,39 @@ sub ical_for_dom {
 
     my $nevents = 0;
 
-    print STDERR "Max week is $maxweek\n";
-
-    ($year, $month, $day) = Add_Delta_Days( $year, $month, $day, 7 * ($minweek - 1) );
-
     # put the events in the calendar
-    #for (my $w = $minweek; $w <= $maxweek; $w++) {
-        for (my $d = 1; $d <= 5; $d++) {
-            my @events = @{ $events[$d] };
+    foreach my $event (@events) {
+        my @exdates;
+        my ($thisyear, $thismonth, $thisday) = Add_Delta_Days( $year, $month, $day, $event->{dayofweek} );
+        my $startdate = undef;
+        my $nweeks = 0;
+        my $minweek = min( $event->{weeks}->range );
+        my $maxweek = max( $event->{weeks}->range );
 
-            EVENT:
-            foreach my $event (@events) {
-                my @recur;
-                my @exdates;
-                my ($thisyear, $thismonth, $thisday) = ($year, $month, $day);
-                my $startdate = undef;
-                my $nweeks = 0;
+        for (my $w = $minweek; $w <= $maxweek; $w++) {
+            my ($hours, $minutes) = split /:/, $event->{time};
+            my $icaldate = sprintf( "%04d%02d%02dT%02d%02d00Z", _utc_time_for( $thisyear, $thismonth, $thisday, $w, $hours, $minutes ) );
 
-                WEEK:
-                for (my $w = $minweek; $w <= $maxweek; $w++) {
-
-                    # make a date object
-                    # TODO: only do this for startdate and exdates
-                    my ($hours, $minutes) = split /:/, $event->{time};
-                    my $localdate = DateTime->new(
-                            year => $thisyear,
-                            month => $thismonth,
-                            day => $thisday,
-                            hour => $hours,
-                            minute => $minutes,
-                            time_zone => 'Europe/London',
-                    );
-                    $localdate->set_time_zone( 'UTC' );
-                    $thisyear = $localdate->year;
-                    $thismonth = $localdate->month;
-                    $thisday = $localdate->day;
-                    $hours = $localdate->hour;
-                    $minutes = $localdate->minute;
-                    my $icaldate = sprintf( "%04d%02d%02dT%02d%02d00Z", $thisyear, $thismonth, $thisday, $hours, $minutes );
-
-                    if (!defined $startdate) {
-                        $startdate = $icaldate;
-                        $nweeks = 1;
-                    }
-
-                    $nweeks++;
-
-                    if (!$event->{weeks}->inrange( $w )) {
-                        push @exdates, $icaldate;
-                    }
-
-                    ($thisyear, $thismonth, $thisday) = Add_Delta_Days( $thisyear, $thismonth, $thisday, 7 );
-                }
-
-                # add an event hash
-                my %icalevent = (
-                        DTSTART => $startdate,
-                        DURATION => ($event->{hours} * 60 - 10) . "M",
-                        EXDATE => join( ',', @exdates ),
-                        RRULE => "FREQ=WEEKLY;COUNT=$nweeks",
-                        SUMMARY => "$event->{subject} $event->{room}",
-                );
-                push @ical, \%icalevent;
-                $nevents++;
+            if ($event->{weeks}->inrange( $w )) {
+                $startdate = $icaldate if !defined $startdate;
+            } else {
+                push @exdates, $icaldate if defined $startdate;
             }
 
-            # go to next day
-            ($year, $month, $day) = Add_Delta_Days( $year, $month, $day, 1 );
+            $nweeks++ if defined $startdate;
         }
-    #}
 
+        # add an event hash
+        my %icalevent = (
+                DTSTART => $startdate,
+                DURATION => ($event->{hours} * 60 - 10) . "M",
+                RRULE => "FREQ=WEEKLY;COUNT=$nweeks",
+                SUMMARY => "$event->{subject} $event->{room}",
+        );
+        $icalevent{EXDATE} = join( ',', @exdates) if @exdates;
+        push @ical, \%icalevent;
+        $nevents++;
+    }
     print STDERR "Write $nevents events\n";
 
     return \@ical;
